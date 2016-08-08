0cefd51611056d6821b305c126e446cc93324929
RetroCraft <retrocraft314@gmail.com>
2016-08-08 11:29:49 -0400

    Add config folder -some stuff

---
+++ .config/thefuck/settings.py
@@ -0,0 +1,20 @@
+# The Fuck settings file
+#
+# The rules are defined as in the example bellow:
+#
+# rules = ['cd_parent', 'git_push', 'python_command', 'sudo']
+#
+# The default values are as follows. Uncomment and change to fit your needs.
+# See https://github.com/nvbn/thefuck#settings for more information.
+#
+
+# history_limit = None
+# env = {'LANG': 'C', 'GIT_TRACE': '1', 'LC_ALL': 'C'}
+# debug = False
+# alter_history = True
+# rules = [<const: All rules enabled>]
+# require_confirmation = True
+# priority = {}
+# no_colors = False
+# exclude_rules = []
+# wait_command = 3
